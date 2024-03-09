e73d4e488a0e64782e503b29485e4a7e667eeb36
Sohrab Chegini <sohrab.chegini@gmail.com>
2024-03-09 21:39:27 +0330

    feat: setup path aliasing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
+import "@/styles/index.css";
 
 const rootElement = document.getElementById("app")!;
 
