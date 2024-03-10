1e8029dc8ecd1f59ea5dec8b7ae084f7f87994c7
Sohrab Chegini <sohrab.chegini@gmail.com>
2024-03-10 11:56:23 +0330

    feat: setup eslint import plugins

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import "@/styles/index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@/styles/index.css";
-import Providers from "./providers";
+
+import Providers from "@/providers";
 
 const rootElement = document.getElementById("app")!;
 
